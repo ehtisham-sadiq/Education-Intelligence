66de7fb2f8478b2f8449d007ef9dd27341a2a8f8
Mahnoor Qasim <mahnoorqasim567@gmail.com>
2024-04-28 01:08:17 -0700

    App.js file updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Ehtisham Sadiq</h1>
+      <h1>Mahnoor Qasim</h1>
+      <p>I have habit of reading novels.I like urdu novels more.I also have interest
+        in historical places and books.I want to become an independend women both financilly
+        and mentally.I want to fulfill my wishes on my own. I want to support my self on my 
+        own. I have greatest wish to sponser my mother for her umrah. Than my second bigest wish
+        is to perform umraah by my self, that is why i am trying to do good for my career. 
+        
+      </p>
+      <img src="https://marketplace.canva.com/EAFIddqdjTk/2/0/1600w/canva-black-minimalist-motivation-quote-linkedin-banner-1PLNOKlL1HU.jpg"></img>
     </div>
   );
 }
